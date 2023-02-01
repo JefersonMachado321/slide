f143c24365797825972ecd072d291afff2605dfe
Jeferson Machado <jefersonmachado321@gmail.com>
2023-02-01 20:55:11 -0300

    custom control estilizados

--- js/slide.js
+++ js/slide.js
@@ -149,7 +149,7 @@ export  class Slide{
   }
 };
 
-export class SlideNav extends Slide{
+export default class SlideNav extends Slide{
   constructor(slide ,wrapper){
    super(slide ,wrapper);
    this.bindControlEvents();
@@ -199,5 +199,4 @@ export class SlideNav extends Slide{
     this.eventControl = this.eventControl.bind(this);
     this.activeControlItem = this.activeControlItem.bind(this);
   }
-
 }
